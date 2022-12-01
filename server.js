a350a25f0dcd1c000c70124bc31b7b18ee752f39
Gary N <gary.newton@ymail.com>
2022-12-01 09:26:58 -0500

    Greeting express application created.

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get("/greeting/:name", function (req, res) {
     res.send("Wow! Hello there, " + req.params.name);
 });
 
+app.get("/tip/:total/:tipPercentage", function (req, res) {
+    let total = req.params.total;
+    let tipPercentage = req.params.tipPercentage/100;
+    let tip = total * tipPercentage
+    res.send("Wow! Hello there, " + req.params.name);
+});
+
 app.listen(PORT, function () {
     console.log("Listening on port: " + PORT);
-  });
+});
