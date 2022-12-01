292cb24e5eb5dba0a86aca5f331e1fd99d72a8eb
Gary N <gary.newton@ymail.com>
2022-12-01 11:33:25 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -22,6 +22,41 @@ app.get("/tip/:total/:tipPercentage", function (req, res) {
              );
 });
 
+
+app.get('/magic/:question', function (req, res) {
+    let randomReplies = [
+        "It is certain", 
+        "It is decidedly so", 
+        "Without a doubt", 
+        "Yes definitely",
+        "You may rely on it", 
+        "As I see it yes", 
+        "Most likely", 
+        "Outlook good", 
+        "Yes", 
+        "Signs point to yes", 
+        "Reply hazy try again", 
+        "Ask again later", 
+        "Better not tell you now", 
+        "Cannot predict now", 
+        "Concentrate and ask again", 
+        "Don't count on it", 
+        "My reply is no", 
+        "My sources say no", 
+        "Outlook not so good", 
+        "Very doubtful"
+    ]
+    let  getRndInteger = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+    let randomReply = randomReplies[getRndInteger(1,20)-1]
+    let question = req.params.question.replace("%20", " ")
+    res.send("You asked: " + question + " The spirit says..." + randomReply);
+});
+
+
+
 app.listen(PORT, function () {
     console.log("Listening on port: " + PORT);
 });
+
